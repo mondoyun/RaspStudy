72ea819a227a5c1362a425b1e22a03c3aab1b0d4
yoeliem <yoeliem29@gmail.com>
2024-07-02 15:48:57 +0900

    rest

--- led.py
+++ led.py
@@ -1,7 +1,10 @@
 import serial
 import time
 
-class LED_Power_ON:
+power_on_command = b'\x7E\x01\xFE\xFE\x00\x0B\x50\x57\x4F\x4E\x02\x00\x00\x00\x00\x00\x00\xFF\xFF\x7E\x00'
+power_off_command = b'\x7E\x01\xFE\xFE\x00\x0B\x50\x57\x4F\x4E\x03\x00\x00\x00\x00\x00\x00\xFF\xFF\x7E\x00'
+
+class LED:
     def __init__(self):
 
         self.port = "/dev/ttyUSB0"
@@ -10,24 +13,22 @@ class LED_Power_ON:
         
     def turn_on(self):
         # 전원을 켜는 명령 전송
-        power_on_command = b'\x7E\x01\xFE\xFE\x00\x0B\x50\x57\x4F\x4E\x02\x00\x00\x00\x00\x00\x00\xFF\xFF\x7E\x00'
         self.serial.write(power_on_command)
         print("LED 전광판 전원을 켰습니다.")
 
     def turn_off(self):
         # 전원을 끄는 명령 전송
-        power_off_command = b'\x7E\x01\xFE\xFE\x00\x0B\x50\x57\x4F\x4E\x03\x00\x00\x00\x00\x00\x00\xFF\xFF\x7E\x00'
         self.serial.write(power_off_command)
         print("LED 전광판 전원을 껐습니다.")
 
     def close(self):
         self.serial.close()        
 # LED 전광판 객체 생성    
-LED = LED_Power_ON()
+LED = LED()
 
 # LED 전원 켜기
 LED.turn_on()
-time.sleep(5)
+time.sleep(4) # 4초 대기
 
 # LED 전원 끄기
 LED.turn_off()
