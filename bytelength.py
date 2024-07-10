0221b15cc6116a635bb7adf165514f6ff8c55b6a
yoeliem <yoeliem29@gmail.com>
2024-07-10 18:28:25 +0900

    test

--- bytelength.py
+++ bytelength.py
@@ -18,25 +18,31 @@
 # #byte_data = ''.join([f'\\x{ord(char):02x}' for char in en_str])
 # # 결과 출력
 # #print(byte_data)
-
+from file import Protocol
 # 한글 문자열
-hangul_str = '안녕하세요'
+koneanSTR = '안녕하세요'
 
 # EUC-KR로 인코딩한 바이트 데이터
-encoded_bytes = hangul_str.encode('euc-kr')
+encodingByte = koneanSTR.encode('euc-kr')
+hex_representation = ' '.join([f'{byte:02X}' for byte in encodingByte])
+print("인코딩데이터 : ", hex_representation)
+Protocol.self.FixedEventText1() + Protocol.self.sendEventText() + Protocol.self.FixedEventText2() + Protocol.TotalSendEventText()
+# encodingBytelen = len(encodingByte)
+# print("데이터의 길이 : ", encodingBytelen)
+# # 바이트 데이터와 더하기
+# originalByte = b'Some bytes data '
+# originalBytelen = len(originalByte)
+# print("데이터의 길이 : ", originalBytelen)
+# result_bytes = originalByte + encodingByte
+# print("더하기 결과 : ",result_bytes)
+# # 결과의 길이
+# result_length = len(result_bytes)
+# print("더하기 길이 결과 : ", result_length)
+# # 바이트 데이터의 hex 값으로 표시
+# hex_representation = ' '.join([f'{byte:02X}' for byte in result_bytes])
 
-# 바이트 데이터와 더하기
-original_bytes = b'Some bytes data '
-result_bytes = original_bytes + encoded_bytes
-
-# 결과의 길이
-result_length = len(result_bytes)
-
-# 바이트 데이터의 hex 값으로 표시
-hex_representation = ' '.join([f'{byte:02X}' for byte in result_bytes])
-
-# 결과 출력
-print(f"Result length: {result_length} bytes")
-print(f"Hex representation: {hex_representation}")
+# # 결과 출력
+# print(f"결과 길이: {result_length} bytes")
+# print(f"16진수로 변환한 값: {hex_representation}")
 
 
